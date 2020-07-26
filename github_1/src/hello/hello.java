2c7b41c0e717ef1679fec350b0a34d7e8114d247
admin <admin@admins-mbp-3>
2020-07-26 16:51:20 -0400

    it is hello examples..

---
+++ github_1/src/hello/hello.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloEclipse World");
+		System.out.println("learning Rest-Assured is very useful for us");
+		System.out.println("Hello Jenkins");
+		
+
+	}
+
+}
