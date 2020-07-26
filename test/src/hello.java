a924e712b8ce9a916ac4be0af5ba6b8f9c460547
admin <admin@admins-mbp-3>
2020-07-26 17:02:17 -0400

    This is an example...

---
+++ test/src/hello.java
@@ -0,0 +1,11 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello class");
+		System.out.println("hello Jenkins");
+		// TODO Auto-generated method stub
+
+	}
+
+}
